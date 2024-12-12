fa65047ee36a2a3d15a0f40f24cebd84c1d53535
lingbright <123716606+lingbright@users.noreply.github.com>
2024-12-12 11:14:35 +0800

    aboutdialog.cpp

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,15 +1,1 @@
-#include "aboutdialog.h"
-#include "ui_aboutdialog.h"
 
-AboutDialog::AboutDialog(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::AboutDialog)
-{
-    ui->setupUi(this);
-    setWindowTitle("关于");
-}
-
-AboutDialog::~AboutDialog()
-{
-    delete ui;
-}
